417a7504c842e6208ba308e0236cd41ac1d9c1d6
Manon <manonlefkos@gmail.com>
2023-05-10 09:08:07 +0200

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,20 @@ output: {
   path: path.resolve(__dirname, "dist"),
 },
 mode: 'development',
+module: {
+  rules: [
+    {
+      test: /\.(?:js|mjs|cjs)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['@babel/preset-env', { targets: "defaults" }]
+          ]
+        }
+      }
+    }
+  ]
+}
 };
